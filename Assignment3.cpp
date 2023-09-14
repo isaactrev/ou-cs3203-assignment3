12e1a67b9f4cb82eb9aaefdbc0ec5e15d6530d15
Isaac Trevino <itrev@hotmail.com>
2023-09-13 22:34:02 -0500

    Adding Assignment3.cpp function for adding an array and returning sum

---
+++ Assignment3.cpp
@@ -0,0 +1,18 @@
+// Assignment3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int sumArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main()
+{
+    cout << "Hello World!\n";
+}
